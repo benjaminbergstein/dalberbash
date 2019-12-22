f1027e0aef1a2dd722595fa4b14376bf414f70ab
Benjamin Bergstein <bennyjbergstein+github@gmail.com>
2019-12-22 21:09:28 +0000

    Document each API endpoint and add POST /players endpoint.

--- src/server.js
+++ src/server.js
@@ -6,12 +6,16 @@ const app = express();
 const EXTRACT_GAME_ID = /^\/games\/([a-zA-Z0-0-]+)$/;
 
 const games = {};
+const gamePlayers = {};
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.static('build'));
 
+// GET /games
+// List games accepting players
+//
 app.get('/games', (req, res) => {
   res.send(Object.entries(games).reduce((g, [name, game]) => {
     const { state } = game;
@@ -20,8 +24,48 @@ app.get('/games', (req, res) => {
   }, {}));
 });
 
+// POST /join
+// Join a specific game
+//
+// @bodyParam name:string - The name of the game
+//
+app.post(/\/join/, (req, res) => {
+  const { name } = req.body;
+  const game = games[name];
+  const { players } = game;
+  const currentPlayer = players + 1;
+  const updatedGame = {
+    ...game,
+    name,
+    players: players + 1,
+    currentPlayer,
+  };
+  games[name] = updatedGame;
+
+  res.send(updatedGame);
+});
+
+const extractGameIdFromPath = (req) => req.path.match(EXTRACT_GAME_ID)[1];
+
+// POST /games/:gameId
+// Create game
+//
+// @urlParam gameId:string
+//
+app.post(/\/games/, (req, res) => {
+  const gameId = extractGameIdFromPath(req);
+  games[gameId] = {
+    name: gameId,
+    ...req.body,
+  };
+  res.send('ok');
+});
+
+// GET /games/:gameId
+// Fetch info about a specific game
+//
 app.get(/\/games/, (req, res) => {
-  const gameId = req.path.match(EXTRACT_GAME_ID)[1];
+  const gameId = extractGameIdFromPath(req);
   const game = games[gameId];
 
   if (game) {
@@ -32,6 +76,28 @@ app.get(/\/games/, (req, res) => {
   res.sendStatus(404);
 });
 
+// POST /players
+// Update game player's name
+//
+// @bodyParam name:string - The name of the game
+// @bodyParam player:integer - Which player to update
+// @bodyParam playerName:string - The new name for the player
+//
+app.post(/\/players/, (req, res) => {
+  const { name, player, playerName } = req.body;
+  const gamePlayers = gamePlayers[name];
+  gamePlayers[name] {
+    ...gamePlayers,
+      [player]: playerName,
+  };
+  res.send('ok');
+});
+
+// POST /start
+// Trigger a game to start
+//
+// @bodyParam name:string - The name of the game
+//
 app.post(/\/start/, (req, res) => {
   const { name } = req.body;
   const game = games[name];
@@ -43,25 +109,13 @@ app.post(/\/start/, (req, res) => {
   res.send('ok');
 });
 
-app.post(/\/vote/, (req, res) => {
-  const { name, player, vote } = req.body;
-  const game = games[name];
-  const { round } = game;
-  const { votes } = round;
-  const updatedGame = {
-    ...game,
-    round: {
-      ...round,
-      votes: {
-        ...votes,
-        [player]: vote,
-      },
-    },
-  };
-  games[name] = updatedGame;
-  res.send('ok');
-});
-
+// POST /answer
+// Submit an answer for the current round
+//
+// @bodyParam name:string - The name of the game
+// @bodyParam player:integer - The ID of the player answering
+// @bodyParam answer:string - Text of the answer
+//
 app.post(/\/answer/, (req, res) => {
   const { name, player, answer } = req.body;
   const game = games[name];
@@ -81,28 +135,29 @@ app.post(/\/answer/, (req, res) => {
   res.send('ok');
 });
 
-app.post(/\/join/, (req, res) => {
-  const { name } = req.body;
+// POST /vote
+// Submit a vote for the current round
+//
+// @bodyParam name:string - The name of the game
+// @bodyParam player:integer - The ID of the player voting
+// @bodyParam vote:integer - The ID of voted answer
+//
+app.post(/\/vote/, (req, res) => {
+  const { name, player, vote } = req.body;
   const game = games[name];
-  const { players } = game;
-  const currentPlayer = players + 1;
+  const { round } = game;
+  const { votes } = round;
   const updatedGame = {
     ...game,
-    name,
-    players: players + 1,
-    currentPlayer,
+    round: {
+      ...round,
+      votes: {
+        ...votes,
+        [player]: vote,
+      },
+    },
   };
   games[name] = updatedGame;
-
-  res.send(updatedGame);
-});
-
-app.post(/\/games/, (req, res) => {
-  const gameId = req.path.match(EXTRACT_GAME_ID)[1];
-  games[gameId] = {
-    name: gameId,
-    ...req.body,
-  };
   res.send('ok');
 });
 
